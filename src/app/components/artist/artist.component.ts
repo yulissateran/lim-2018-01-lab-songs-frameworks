af044aae6dd7b1769e345cff9edf65fc41cced6e
yulissateran <yulissa.lteran@gmail.com>
2019-11-07 20:32:07 -0500

    update

--- src/app/components/artist/artist.component.ts
+++ src/app/components/artist/artist.component.ts
@@ -2,11 +2,15 @@ import { Component, OnInit } from '@angular/core';
 
 import { LastFmService } from '../../services/last-fm.service';
 import { LocalApiService } from '../../services/local-api/local-api.service';
+
+
 @Component({
   selector: 'app-artist',
   templateUrl: './artist.component.html',
   styleUrls: ['./artist.component.css']
 })
+
+
 export class ArtistComponent implements OnInit {
   constructor(
     private api: LocalApiService,
